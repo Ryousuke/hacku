1dfa46e85d2909f588edc247507b78422efb0be6
Ryousuke <115251760+Ryousuke@users.noreply.github.com>
2023-12-10 21:45:56 +0900

    Update app.py

--- production/app/app.py
+++ production/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,session,redirect,url_for,jsonify
 from flask_cors import CORS
 from dataclasses import dataclass
-import sqlite3,json
+import sqlite3
 import secrets
 
 #Flaskオブジェクトの作成
@@ -12,6 +12,7 @@ app.secret_key = secrets.token_hex(16)
 
 @dataclass
 class InfomationsProfessor:
+    professor_id:int
     name:str
     hometown:str
     hobby1:str
@@ -24,26 +25,26 @@ class InfomationsProfessor:
     music:str
     
     def get_attributes(self):
-        return [self.name, self.hometown, self.hobby1, self.hobby2, self.campus, self.faculity, self.team, self.field, self.animal, self.music]
+        return [self.professor_id,self.name, self.hometown, self.hobby1, self.hobby2, self.campus, self.faculity, self.team, self.field, self.animal, self.music]
 
 professors = [
-    InfomationsProfessor("青木文昭", "山梨県", "将棋", "競馬", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "中日ドラゴンズ", "財政学", "ヒツジ", "尾崎豊"),
-    InfomationsProfessor("鈴木百音", "愛知県", "料理", "テニス", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "なし", "公共政策学", "イヌ", "嵐"),
-    InfomationsProfessor("武田勝治", "静岡県", "ベース", "筋トレ", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "なし", "人文地理学", "ネコ", "ロック"),
-    InfomationsProfessor("鋤柄美紀", "広島県", "野球観戦", "コーヒー作り", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "広島東洋カープ", "応用数学", "ネコ", "Mr.Children"),
-    InfomationsProfessor("宮木由賀", "静岡県", "株", "料理", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "なし", "経済", "シカ", "三浦大知"),
-    InfomationsProfessor("青林広高", "秋田県", "麻雀", "盆栽", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "広島東洋カープ", "財政学", "イヌ", "北島三郎"),
-    InfomationsProfessor("柳原健", "三重県", "電車", "野球", "天白キャンパス", "情報工学部情報工学科", "オリックス・バファローズ", "バーチャルリアリティ", "ネコ", "Little Green Monster"),
-    InfomationsProfessor("北中潤", "愛知県", "サッカー", "旅行", "天白キャンパス", "理工学部機械工学科", "なし", "ロボットの知能化", "イヌ","ゆず"),
-    InfomationsProfessor("田中敏夫", "愛知県", "ドライブ", "写真撮影", "天白キャンパス", "情報工学部情報工学科", "オリックス・バファローズ", "コンピュータグラフィックス", "パンダ", "ヒップホップ"),
-    InfomationsProfessor("宇佐見五郎", "愛知県", "釣り", "筋トレ", "天白キャンパス", "情報工学部情報工学科", "北海道日本ハムファイターズ", "符号理論", "クジャク", "ハウス"),
-    InfomationsProfessor("中西淳平", "三重県", "登山", "バドミントン", "天白キャンパス", "理工学部機械工学科", "横浜DeNAベイスターズ", "ロボティクス", "イヌ", "EDM"),
-    InfomationsProfessor("松原剛", "鹿児島県", "ギター", "ダーツ", "天白キャンパス", "理工学部機械工学科", "中日ドラゴンズ", "疲労強度設計", "ゴリラ", "雅楽"),
+    InfomationsProfessor(1,"青木文昭", "山梨県", "将棋", "競馬", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "中日ドラゴンズ", "財政学", "ヒツジ", "尾崎豊"),
+    InfomationsProfessor(2,"鈴木百音", "愛知県", "料理", "テニス", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "なし", "公共政策学", "イヌ", "嵐"),
+    InfomationsProfessor(3,"武田勝治", "静岡県", "ベース", "筋トレ", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "なし", "人文地理学", "ネコ", "ロック"),
+    InfomationsProfessor(4,"鋤柄美紀", "広島県", "野球観戦", "コーヒー作り", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "広島東洋カープ", "応用数学", "ネコ", "Mr.Children"),
+    InfomationsProfessor(5,"宮木由賀", "静岡県", "株", "料理", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "なし", "経済", "シカ", "三浦大知"),
+    InfomationsProfessor(6,"青林広高", "秋田県", "麻雀", "盆栽", "名古屋ドーム前キャンパス", "都市情報学部都市情報学科", "広島東洋カープ", "財政学", "イヌ", "北島三郎"),
+    InfomationsProfessor(7,"柳原健", "三重県", "電車", "野球", "天白キャンパス", "情報工学部情報工学科", "オリックス・バファローズ", "バーチャルリアリティ", "ネコ", "Little Green Monster"),
+    InfomationsProfessor(8,"北中潤", "愛知県", "サッカー", "旅行", "天白キャンパス", "理工学部機械工学科", "なし", "ロボットの知能化", "イヌ","ゆず"),
+    InfomationsProfessor(9,"田中敏夫", "愛知県", "ドライブ", "写真撮影", "天白キャンパス", "情報工学部情報工学科", "オリックス・バファローズ", "コンピュータグラフィックス", "パンダ", "ヒップホップ"),
+    InfomationsProfessor(10,"宇佐見五郎", "愛知県", "釣り", "筋トレ", "天白キャンパス", "情報工学部情報工学科", "北海道日本ハムファイターズ", "符号理論", "クジャク", "ハウス"),
+    InfomationsProfessor(11,"中西淳平", "三重県", "登山", "バドミントン", "天白キャンパス", "理工学部機械工学科", "横浜DeNAベイスターズ", "ロボティクス", "イヌ", "EDM"),
+    InfomationsProfessor(12,"松原剛", "鹿児島県", "ギター", "ダーツ", "天白キャンパス", "理工学部機械工学科", "中日ドラゴンズ", "疲労強度設計", "ゴリラ", "雅楽"),
 ]
 
 def get_professor_by_id(professor_id):
-    for professorin professors:
-        if professorin professors:
+    for professor in professors:
+        if professor.professor_id == professor_id:
             return professor
     return None
 
@@ -62,12 +63,51 @@ def login_page():
     csrf_token = secrets.token_hex(16)
     session['csrf_token'] = csrf_token
     return render_template("login.html",csrf_token=csrf_token)
+    
 @app.route("/welcome")
 def welcome():
     if 'user_id' in session:
         return render_template("welcome.html")
     else:
         return redirect(url_for("login"))
+
+@app.route("/member1")
+def member1():
+    return render_template("question.html")
+@app.route("/member2")
+def member2():
+    return render_template("question.html")
+@app.route("/member3")
+def member3():
+    return render_template("question.html")
+@app.route("/member4")
+def member4():
+    return render_template("question.html")
+@app.route("/member5")
+def member5():
+    return render_template("question.html")
+@app.route("/member6")
+def member6():
+    return render_template("question.html")
+@app.route("/member7")
+def member7():
+    return render_template("question.html")
+@app.route("/member8")
+def member8():
+    return render_template("question.html")
+@app.route("/member9")
+def member9():
+    return render_template("question.html")
+@app.route("/member10")
+def member10():
+    return render_template("question.html")
+@app.route("/member11")
+def member11():
+    return render_template("question.html")
+@app.route("/member12")
+def member12():
+    return render_template("question.html")
+    
 class userMatch:
     def __init__(self,db_name='user_db.db'):
         self.conn=sqlite3.connect(db_name)
@@ -88,7 +128,26 @@ class userMatch:
     
     def close_connection(self):
         self.conn.close()
+class reviewMatch:
+    def __init__(self,db_name='review_db.db'):
+        self.conn=sqlite3.connect(db_name)
+        self.cursor=self.conn.cursor()
+    
+    def create_review_comments(self,user_id,professor_number,comment):
+        self.cursor.execute('''
+            INSERT INTO review_comments(user_id,professor_number,comment) VALUES (?,?,?)
+        ''',(user_id,professor_number,comment))
+        self.conn.commit()
         
+    def get_review_comments(self,professor_number):
+        self.cursor.execute('''
+            SELECT * FROM review_comments WHERE professor_number = ? 
+            ''',(professor_number,))
+        review=self.cursor.fetchall()
+        return review
+        
+    def close_connection(self):
+        self.conn.close()         
         
 @app.route("/login",methods=["POST","GET"])
 def login():
@@ -160,6 +219,7 @@ def result():
         
         best_professor = professors[common_hobbies_count.index(max(common_hobbies_count))]
        data = {
+           'professor_id': best_professor.professor_id,
             'professor_name': best_professor.name,
             'compatibility_score': compatibility_score
         }
@@ -170,7 +230,6 @@ def result():
     
 @app.route('/search',methods=['POST'])
 def search():
-    def search():
     keyword = request.form.get('keyword', '')
     matching_professors = [professor for professor in professors if keyword.lower() in professor.name.lower() or keyword in professor.hometown.lower() or keyword in professor.hobby1.lower() or keyword in professor.hobby2.lower() or keyword in professor.campus.lower() or keyword in professor.faculity.lower() or keyword in professor.team.lower() or keyword in professor.field.lower() or keyword in professor.animal.lower()or keyword in professor.music.lower()]
     return render_template('search_result.html', professors=matching_professors, keyword=keyword)
@@ -202,6 +261,49 @@ def professor_detail(professor_id):
         return render_template('Professor Profile Kakimoto.html', professor=professor)
     else:
         return render_template('welcome.html', message='教授が見つかりません')
-    
+
+@app.route('/comment_contents/<int:professor_id>',methods=['GET','POST'])
+def comment_contents(professor_id):
+    professor=get_professor_by_id(professor_id)
+    reviews=[]
+    if professor:
+        if request.method=='POST':
+            user_id=request.form['user_id']
+            professor_number=request.form['professor_number']
+            comment=request.form['comment']
+            match=reviewMatch()
+            match.create_review_comments(user_id,professor_number,comment)
+            match.close_connection()
+            
+            match=reviewMatch()
+            reviews_data=match.get_review_comments(professor_number)
+            match.close_connection()
+            
+            for review_data in reviews_data:
+                reviews.append({'user_id': review_data[1], 'professor_number': review_data[2], 'comment': review_data[3]})
+    if professor_id==1:
+        return render_template('Professor Profile Kakimoto.html', professor=professor)
+    elif professor_id==2:
+        return render_template('welcome.html', message='教授が見つかりません')
+    elif professor_id==3:
+        return render_template('Professor Profile Kakimoto.html', professor=professor)
+    elif professor_id==4:
+        return render_template('welcome.html', message='教授が見つかりません')
+    elif professor_id==5:
+        return render_template('Professor Profile Kakimoto.html', professor=professor)
+    elif professor_id==6:
+        return render_template('welcome.html', message='教授が見つかりません')
+    elif professor_id==7:
+        return render_template('Professor Profile Kakimoto.html', professor=professor)
+    elif professor_id==8:
+        return render_template('welcome.html', message='教授が見つかりません')
+    elif professor_id==9:
+        return render_template('Professor Profile Kakimoto.html', professor=professor)
+    elif professor_id==10:
+        return render_template('Professor Profile Kakimoto.html', professor=professor,reviews=reviews)
+    elif professor_id==11:
+        return render_template('Professor Profile Kakimoto.html', professor=professor)
+    else:
+        return render_template('welcome.html', message='教授が見つかりません')
 if __name__=="__main__":
     app.run(debug=True)
